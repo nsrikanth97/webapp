cae95ffa038d4e8a4a1642b8375e0ef67ca6eb12
srikanth.n <srikanth.na@northeastern.edu>
2023-10-29 22:07:00 -0400

    Logging added, Updated gitignore

--- src/main/java/edu/neu/csye6225/controller/AssignmentController.java
+++ src/main/java/edu/neu/csye6225/controller/AssignmentController.java
@@ -9,6 +9,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.ConstraintViolation;
 import jakarta.validation.Valid;
 import jakarta.validation.Validator;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,6 +22,7 @@ import java.util.*;
 
 @RestController
 @RequestMapping("v1/assignments")
+@Slf4j
 public class AssignmentController {
 
 
@@ -40,36 +42,54 @@ public class AssignmentController {
     @GetMapping
     @AuthenticateRequest
     public ResponseEntity<Object> getAllAssignments(@RequestBody(required = false) Object body){
+        UUID loggedInUserId = (UUID) request.getSession().getAttribute("accountId");
+        log.info("AssignmentController:getAllAssignments:-Request received to get all assignments for user: {}", loggedInUserId);
         if(body != null || StringUtils.hasLength(request.getQueryString())) {
+            log.error("AssignmentController:getAllAssignments:-Invalid request parameters or body present. Returning Bad Request status.");
+            if(body != null)
+                log.debug("AssignmentController:getAllAssignments:-Request body to get all assignments from the user with id " + loggedInUserId + " is invalid. It has body" );
+            if(StringUtils.hasLength(request.getQueryString()))
+                log.debug("AssignmentController:getAllAssignments:-Request body to get all assignments from the user with id " + loggedInUserId + " is invalid. It has Query params," );
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
         }
         List<Assignment> assignmentList = assignmentService.getAll();
         if(assignmentList.isEmpty()){
+            log.warn("AssignmentController:getAllAssignments:-No assignments found in the system.");
             ResponseEntity.status(404);
         }
+        log.info("AssignmentController:getAllAssignments:-Returning {} assignments for the user: {}", assignmentList.size(), loggedInUserId);
+        log.debug("AssignmentController:getAllAssignments:-Assignments returned are: {}", assignmentList);
         return ResponseEntity.ok(assignmentList);
     }
 
     @GetMapping("/{id}")
     @AuthenticateRequest
     public ResponseEntity<Object> getAssignmentById(@PathVariable UUID id, @RequestBody(required = false) Object body){
+        log.info("AssignmentController:getAssignmentById:-Request received to fetch assignment by ID: {}", id);
         if(body != null || StringUtils.hasLength(request.getQueryString())) {
+            log.error("AssignmentController:getAssignmentById:-Invalid request parameters or body present for assignment ID: {}. Returning Bad Request status.", id);
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
         }
         Response<Assignment> assignmentResponse = assignmentService.getAssignmentById(id);
         if(assignmentResponse.getStatus().equals(Response.ReturnStatus.SUCCESS)){
+            log.info("AssignmentController:getAssignmentById:-Assignment with ID: {} found in the system.", id);
             return  ResponseEntity.ok(assignmentResponse.getData());
         }
+        log.warn("AssignmentController:getAssignmentById:-Assignment not found for ID: {}. Returning Not Found status.", id);
         return ResponseEntity.status(404).body(assignmentResponse);
     }
 
     @PostMapping
     @AuthenticateRequest
     public ResponseEntity<Object> createAssignment(@RequestBody(required = false) Assignment assignment){
+        UUID loggedInUserId = (UUID) request.getSession().getAttribute("accountId");
+        log.info("AssignmentController:createAssignment:-Request received to create an assignment by the user: {}", loggedInUserId);
         if(StringUtils.hasLength(request.getQueryString()) || assignment == null) {
+            log.error("AssignmentController:createAssignment:-Invalid request parameters or empty assignment. Returning Bad Request status.");
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
         }
-
+        log.debug("AssignmentController:createAssignment:-Validating the request body for assignment creation." +
+                " Request body is: {}", assignment);
         Map<String, String> hashMap = new HashMap<>();
         Set<ConstraintViolation<Assignment>> violations = validator.validate(assignment);
         if (!violations.isEmpty()){
@@ -78,16 +98,21 @@ public class AssignmentController {
             Response<Map<String,String>> response = new Response<>();
             response.setStatus(Response.ReturnStatus.FAILURE);
             response.setData(hashMap);
+            log.warn("AssignmentController:createAssignment:-Assignment creation failed for user: {}. Returning Bad Request status.", loggedInUserId);
             return ResponseEntity.badRequest().body(response);
         }
-
+        log.debug("AssignmentController:createAssignment:-Request body is valid. Proceeding with the creation.");
+        log.info("AssignmentController:createAssignment:-Assignment creation successful for user: {}", loggedInUserId);
         return ResponseEntity.status(201).body(assignmentService.save(assignment));
     }
 
     @DeleteMapping("/{id}")
     @AuthenticateRequest
     public ResponseEntity<Object> deleteAssignment(@PathVariable UUID id, @RequestBody(required = false) Object body){
+        UUID loggedInUserId = (UUID) request.getSession().getAttribute("accountId");
+        log.info("AssignmentController:deleteAssignment:-Request received to delete assignment by ID: {} for user: {}", id, loggedInUserId);
         if(body != null || StringUtils.hasLength(request.getQueryString())) {
+            log.error("AssignmentController:deleteAssignment:-Invalid request parameters or body present for assignment ID: {}. Returning Bad Request status.", id);
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
         }
         return assignmentService.deleteAssignment(id);
@@ -97,10 +122,14 @@ public class AssignmentController {
     @AuthenticateRequest
     public ResponseEntity<Object> updateAssignment(@RequestBody(required = false) Assignment assignment,
                                                    @PathVariable UUID id){
+        UUID loggedInUserId = (UUID) request.getSession().getAttribute("accountId");
+        log.info("AssignmentController:updateAssignment:-Request received to update assignment by ID: {} for user: {}", id, loggedInUserId);
         if(StringUtils.hasLength(request.getQueryString()) || assignment == null) {
+            log.error("AssignmentController:updateAssignment:-Invalid request parameters or empty assignment. Returning Bad Request status.");
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
         }
-
+        log.debug("AssignmentController:updateAssignment:-Validating the request body for assignment update." +
+                " Request body is: {}", assignment);
         Map<String, String> hashMap = new HashMap<>();
         Set<ConstraintViolation<Assignment>> violations = validator.validate(assignment);
         if (!violations.isEmpty()){
@@ -109,8 +138,10 @@ public class AssignmentController {
             Response<Map<String,String>> response = new Response<>();
             response.setStatus(Response.ReturnStatus.FAILURE);
             response.setData(hashMap);
+            log.error("AssignmentController:updateAssignment:-Assignment update failed for user: {}. Returning Bad Request status.", loggedInUserId);
             return ResponseEntity.badRequest().body(response);
         }
+        log.debug("AssignmentController:updateAssignment:-Request body is valid. Proceeding with the update.");
         return assignmentService.updateAssignment(id,assignment);
     }
 
--- src/main/java/edu/neu/csye6225/services/HealthCheckService.java
+++ src/main/java/edu/neu/csye6225/services/HealthCheckService.java
@@ -2,10 +2,12 @@ package edu.neu.csye6225.services;
 
 
 import edu.neu.csye6225.repository.DatabaseConnectionRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class HealthCheckService {
 
     @Autowired
@@ -14,8 +16,10 @@ public class HealthCheckService {
     public boolean isDatabaseConnected() {
         try{
             int res = databaseConnectionRepository.checkConnection();
+            log.info("HealthCheckService:isDatabaseConnected:-Database connection check result: " + res);
             return res == 1;
         }catch(Exception ex){
+            log.error("HealthCheckService:isDatabaseConnected:-Exception occurred while checking database connection: " + ex.getMessage());
             return false;
         }
     }
